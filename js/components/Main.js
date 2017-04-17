187361b4e57b5f2c8e20a1a80fa57637ba0a2b35
Noah Coughlan <noah@COUGHLAN-UBUNTU.attlocal.net>
2017-04-17 16:36:44 -0700

    8

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,42 +1,86 @@
 import React from "react";
 import Relay from "react-relay";
+import {debounce} from "lodash";
 
-import Link from './Link';
+import Link from "./Link";
+import CreateLinkMutation from "../mutations/CreateLinkMutation";
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
 
-  static propTypes = {
-    limit: React.PropTypes.number
+    this.search = debounce(this.search, 300);
   }
-
-  static defaultProps = {
-    limit: 5
-  }  
-
+  search = (e) => {
+    let query = e.target.value;
+    this.props.relay.setVariables({ query });
+  }
+  setLimit = (e) => {
+    let newLimit = Number(e.target.value);
+    this.props.relay.setVariables({limit: newLimit});
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    Relay.Store.update(
+      new CreateLinkMutation({
+        title: this.refs.newTitle.value,
+        url: this.refs.newUrl.value,
+        store: this.props.store
+      })
+    );
+    this.refs.newTitle.value = "";
+    this.refs.newUrl.value = "";
+  }
   render() {
-    let content = this.props.store.links.slice(0, this.props.limit).map(link => {
-      return <Link key={link._id} link={link} />;
+    let content = this.props.store.linkConnection.edges.map(edge => {
+      return <Link key={edge.node.id} link={edge.node} />;
     });
 
     return (
       <div>
         <h3>Links</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" ref="newTitle"/>
+          <input type="text" placeholder="Url" ref="newUrl"/>
+          <button type="submit">Add</button>
+        </form>
+        Showing: &nbsp;
+        <input type="text" placeholder="Search" onChange={this._search}/>
+        <select onChange={this.setLimit}
+                defaultValue={this.props.relay.variables.limit}>
+          <option value="5">5</option>
+          <option value="100">100</option>
+        </select>
         <ul>
           {content}
         </ul>
       </div>
     );
   }
+
+  _search = (e) => {
+    e.persist();
+    this.search(e);
+  }
 }
 
 // Declare the data requirement for this component
 Main = Relay.createContainer(Main, {
+  initialVariables: {
+    limit: 100,
+    query: ''
+  },
   fragments: {
     store: () => Relay.QL`
       fragment on Store {
-        links {
-          _id,
-          ${Link.getFragment('link')}
+        id,
+        linkConnection(first: $limit, query: $query) {
+          edges {
+            node {
+              id,
+              ${Link.getFragment('link')}
+            }
+          }
         }
       }
     `
